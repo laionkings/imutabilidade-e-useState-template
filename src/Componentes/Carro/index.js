ee2f8dcc66549393b732385ca4d56a2820ad076a
Laion Pereira <kingslaion@gmail.com>
2023-01-23 12:32:36 -0300

    exercicios de imutabilidade

--- src/Componentes/Carro/index.js
+++ src/Componentes/Carro/index.js
@@ -1,15 +1,25 @@
-import { CarroContainer } from './styles'
+import { CarroContainer } from "./styles";
 
-export function Carro({ modelo, cor, ano, flex, adicionadoPor }) {
+export function Carro({
+  modelo,
+  cor,
+  ano,
+  adicionadoPor,
+  flex,
+  setCarro,
+  novoCarro
+}) {
+  console.log(modelo);
   return (
     <CarroContainer>
       <h2>{modelo}</h2>
       <ul>
         <li>Cor: {cor}</li>
         <li>Ano: {ano}</li>
-        <li>Flex: {flex ? 'Sim' : 'Não'}</li>
+        <li>Flex: {flex ? "Sim" : "Não"}</li>
         <li>Adicionado: {adicionadoPor}</li>
       </ul>
+      <button onClick={() => setCarro(novoCarro)}> Muda carro </button>
     </CarroContainer>
-  )
+  );
 }
